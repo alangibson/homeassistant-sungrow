8bc5a4a955fc6932541c1c21485348bc3628129a
alangibson <alan.gibson@gmail.com>
2024-07-11 15:28:26 +0200

    Change to relative path import

--- custom_components/sungrow/SunGather/inverter.py
+++ custom_components/sungrow/SunGather/inverter.py
@@ -1,7 +1,7 @@
 import logging
 from datetime import datetime
-from SungrowModbusTcpClient import SungrowModbusTcpClient
-from SungrowModbusWebClient import SungrowModbusWebClient
+from ..SungrowModbusTcpClient import SungrowModbusTcpClient
+from ..SungrowModbusWebClient import SungrowModbusWebClient
 from pymodbus.client import ModbusTcpClient
 
 logger = logging.getLogger(__name__)
